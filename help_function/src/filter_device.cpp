b47644b37ae50fddb9f21e18c7e0def4fc70fb21
ziranzha <ziran.zhang@intel.com>
2024-05-08 10:04:03 +0800

    Update help_function/src/filter_device.cpp
    Co-authored-by: Shengchen <sheng.s.chen@intel.com>

--- help_function/src/filter_device.cpp
+++ help_function/src/filter_device.cpp
@@ -1,4 +1,4 @@
-// ====------ fft_set_workspace.cpp ------------------------- *- C++ -* ----===//
+// ====------  filter_device.cpp ------------------------- *- C++ -* ----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
