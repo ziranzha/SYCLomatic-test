3ea579b0f2c1711b856330ff770f18b445409920
Tang Jiajun <jiajun.tang@intel.com>
2024-05-13 13:41:37 +0800

    [SYCLomatic #1763] Modify bindless image helper function test. (#713)
    Signed-off-by: Tang, Jiajun jiajun.tang@intel.com

--- help_function/src/bindless_images.cpp
+++ help_function/src/bindless_images.cpp
@@ -1,4 +1,4 @@
-// ===------------ bindless_images.cpp ---------- -*- C++ -* --------------===//
+// ===------------ bindless_images.cpp ----------- *- C++ -* --------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -24,23 +24,29 @@ int main() {
   m = new dpct::experimental::image_mem_wrapper(c, x);
   m = new dpct::experimental::image_mem_wrapper(c, x, y);
   m = new dpct::experimental::image_mem_wrapper(c, x, y, z);
+  m = new dpct::experimental::image_mem_wrapper(
+      c, sycl::range<3>{4, 4, 4},
+      sycl::ext::oneapi::experimental::image_type::mipmap, (unsigned)2);
   m->get_channel();
   m->get_range();
   m->get_desc();
   m->get_handle();
+  m->get_mip_level(1);
   dpct::image_data d; //(m); // TODO: need support.
   d.set_data(m);
   printf("image mem wrapper pass!\n");
-  dpct::experimental::async_dpct_memcpy(m, 0, 0, f, 1, 1, 1, q);
-  dpct::experimental::dpct_memcpy(m, 0, 0, f, 1, 1, 1, q);
-  dpct::experimental::async_dpct_memcpy(m, 0, 0, f, 1, q);
-  dpct::experimental::dpct_memcpy(m, 0, 0, f, 1, q);
-  dpct::experimental::async_dpct_memcpy(f, m, 0, 0, 1, 1, 1, q);
-  dpct::experimental::dpct_memcpy(f, m, 0, 0, 1, 1, 1, q);
-  dpct::experimental::async_dpct_memcpy(f, m, 0, 0, 1, q);
-  dpct::experimental::dpct_memcpy(f, m, 0, 0, 1, q);
-  dpct::experimental::dpct_memcpy(m, 0, 0, m, 0, 0, 1, 1, q);
-  dpct::experimental::dpct_memcpy(m, 0, 0, m, 0, 0, 1, q);
+  const auto cm = m;
+  const auto cf = f;
+  dpct::experimental::async_dpct_memcpy(f, 1, cm, 0, 0, 1, 1, q);
+  dpct::experimental::dpct_memcpy(f, 1, cm, 0, 0, 1, 1, q);
+  dpct::experimental::async_dpct_memcpy(f, cm, 0, 0, 1, q);
+  dpct::experimental::dpct_memcpy(f, cm, 0, 0, 1, q);
+  dpct::experimental::async_dpct_memcpy(m, 0, 0, cf, 1, 1, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, cf, 1, 1, 1, q);
+  dpct::experimental::async_dpct_memcpy(m, 0, 0, cf, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, cf, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, cm, 0, 0, 1, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, cm, 0, 0, 1, q);
   printf("memory copy pass!\n");
   auto h = dpct::experimental::create_bindless_image(d, samp, q);
   dpct::experimental::get_data(h);
@@ -68,7 +74,25 @@ int main() {
   i.get_filtering_mode();
   i.get_handle();
   printf("texture referece pass!\n");
+  dpct::memcpy_parameter param;
+  dpct::memcpy_parameter::data_wrapper dw;
+  dpct::image_matrix_p im = new dpct::image_matrix(c, sycl::range<3>(1, 1, 1));
+  dw.pitched = dpct::pitched_data(f, 1, 1, 0);
+  dw.pos = sycl::id<3>(0, 0, 0);
+  dw.image_bindless = nullptr;
+  dw.image = nullptr;
+  param.from = dw;
+  param.to.pitched = dpct::pitched_data();
+  param.to.pos = sycl::id<3>(0, 0, 0);
+  param.to.image_bindless = m;
+  param.to.image = im;
+  param.size = sycl::range<3>(1, 1, 0);
+  param.direction = dpct::memcpy_direction::automatic;
+  dpct::async_dpct_memcpy(param, q);
+  dpct::dpct_memcpy(param, q);
+  printf("memcpy parameter pass!\n");
   delete m;
+  delete im;
   printf("test pass!\n");
   return 0;
 }
