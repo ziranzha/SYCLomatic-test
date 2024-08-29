de7c1f5c48e0249c82ba881cc3997817c06d00bf
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-08-29 10:59:48 +0800

    [SYCLomatic] Fix feautres/user_defined_rules (#774)
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- features/feature_case/user_defined_rules/myhelper.h
+++ features/feature_case/user_defined_rules/myhelper.h
@@ -1,5 +1,11 @@
-size_t *foo4(int ***i, int *j, sycl::queue q, sycl::context c, dpct::device_ext& d){return 0;}
+size_t *foo4(int ***i, int *j, sycl::queue q, sycl::context c,
+#if USE_DPCT_HELPER
+             dpct::device_ext &d
+#else
+             syclcompat::device_ext &d
+#endif
+) {
+  return 0;
+}
 
-class mytype{};
-
-
+class mytype {};
